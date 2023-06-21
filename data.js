6e81a16357532af215999c0ed392c1979b478098
sarucodes <koiralasaru420@gmail.com>
2023-06-21 15:55:19 +0545

    commit

--- data.js
+++ data.js
@@ -1,42 +1,8 @@
-
-const file = require("./file");
-const fs = require('fs');
-const { default: axios } = require('axios');
-
 class Data{
     title="";
-    URL="";
+    url="";
+    link="";
     image="";
-    topic="";
-    category="";
-    async getData(){
-        try {
-            const data= await axios.get(this.URL);
-            return data.data;
-            
-        } catch (error) {
-            
-            throw error;
-        }
-    }
-
-    transformData(data){
-        return data;
-    }
-
-    save(data){
-        console.log("saving data");
-        const jsonData = JSON.stringify(data,null, 2);
-        const filePath =this.name+'.json';
-        fs.writeFile(filePath,jsonData, 'utf-8',(err) =>{
-            if (err){
-                console.error('An error occur while writing to the file:', err);
-        
-            }else{
-                console.log('Data has been successfully saved to the file.');
-            }
-        });
-    }
+    topic="loksewa";
+    category="notice";
 }
-
-module.exports=Data;
